18ac3fced8fa21dab0f92ca6ac8555511a91e9e4
Piotr <pececik@gmail.com>
2017-07-12 20:29:56 +0200

    dodanie klasy Klasa

---
+++ src/pl/sdacademy/list/Klasa.java
@@ -0,0 +1,21 @@
+package pl.sdacademy.list;
+
+/**
+ * Created by RENT on 2017-07-12.
+ */
+public class Klasa {
+    public Klasa(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    private int a;
+
+}
